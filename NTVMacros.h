4c4eb45a049f771cc22a861b378ef56994bb2496
Zachary Waldowski <zach@waldowski.me>
2014-08-23 19:32:26 -0400

    Fix floating point conversion, geometry, floating point math warnings

--- NTVMacros.h
+++ NTVMacros.h
@@ -29,4 +29,12 @@ NTV_ALWAYS_INLINE AppController *NTVAppDelegate(void) {
 	return (AppController *)[NSApp delegate];
 }
 
+NTV_ALWAYS_INLINE BOOL NTVFloatsEqual(CGFloat a, CGFloat b) {
+#if CGFLOAT_IS_DOUBLE
+    return fabs(a - b) < DBL_EPSILON;
+#else
+    return fabsf(a - b) < FLT_EPSILON;
+#endif
+}
+
 #endif /* !NTV_MACROS */
